0a45cb5bba49c9cde91fd3d4c149f9ac38ab813d
theharoldroyce <theharoldroyce@yahoo.com>
2023-05-19 15:07:51 +0800

    05232023 -changing socket server

--- src/components/Shop/DashboardMessages.jsx
+++ src/components/Shop/DashboardMessages.jsx
@@ -9,7 +9,7 @@ import styles from "../../styles/styles";
 import { TfiGallery } from "react-icons/tfi";
 import socketIO from "socket.io-client";
 import { format } from "timeago.js";
-const ENDPOINT = "http://localhost:5000/";
+const ENDPOINT = "https://unimak-socket-server.onrender.com/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const DashboardMessages = () => {
