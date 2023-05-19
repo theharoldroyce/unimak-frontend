1b91ed6475a41109ad8c73ed661afcafca7775af
theharoldroyce <theharoldroyce@yahoo.com>
2023-05-19 11:53:09 +0800

    05232023 - changing server routes

--- src/server.js
+++ src/server.js
@@ -1,3 +1,8 @@
-export const server = "http://localhost:4000/api/v2";
+// export const server = "http://localhost:4000/api/v2";
 
-export const backend_url = "http://localhost:4000/";
+// export const backend_url = "http://localhost:4000/";
+
+
+export const server = "https://unimak-backendserver.adaptable.app/api/v2";
+
+export const backend_url = "https://unimak-backendserver.adaptable.app/";
