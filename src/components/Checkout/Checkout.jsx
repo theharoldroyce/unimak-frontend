9ea13130e6d42ce4c0571f64117186a21c8f942d
Harold Royce <104117299+theharoldroyce@users.noreply.github.com>
2023-06-03 00:12:44 +0800

    Update Checkout.jsx

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -97,7 +97,7 @@ const Checkout = () => {
   const discountPercentenge = couponCodeData ? discountPrice : "";
 
   const totalPrice = couponCodeData
-    ? (subTotalPrice - discountPercentenge + 300).toFixed(2)
+    ? (discountPercentenge + 300).toFixed(2)
     : (subTotalPrice + 300).toFixed(2);
 
   console.log(discountPercentenge);
@@ -329,6 +329,12 @@ const CartData = ({
         <h3 className="text-[16px] font-[400] text-[#000000a4]">subtotal:</h3>
         <h5 className="text-[18px] font-[600]">₱ {subTotalPrice}.00</h5>
       </div>
+      <br/>
+      <div className="flex justify-between">
+        <h3 className="text-[16px] font-[400] text-[#000000a4]">Discounted subtotal:</h3>
+        <h5 className="text-[18px] font-[600]">₱ {discountPercentenge}.00</h5>
+      </div>
+
       <br />
       <div className="flex justify-between">
         <h3 className="text-[16px] font-[400] text-[#000000a4]">shipping:</h3>
@@ -338,7 +344,7 @@ const CartData = ({
       <div className="flex justify-between border-b pb-3">
         <h3 className="text-[16px] font-[400] text-[#000000a4]">Discount:</h3>
         <h5 className="text-[18px] font-[600]">
-          - {discountPercentenge ? "₱a" + discountPercentenge.toString() : null}
+          - {discountPercentenge ? "₱" + discountPercentenge.toString() : null}
         </h5>
       </div>
       <h5 className="text-[18px] font-[600] text-end pt-3">₱ {totalPrice}</h5>
