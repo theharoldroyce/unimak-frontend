1c9bd381d02536523c2b626658f29d78a8c1dadb
theharoldroyce <theharoldroyce@yahoo.com>
2023-06-11 07:32:37 +0800

    06102023 -ui update

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,40 +1,44 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import { useSelector } from 'react-redux';
 import styles from '../../styles/styles'
 import EventCard from "./EventCard";
 
 const Events = () => {
-  const {allEvents,isLoading} = useSelector((state) => state.events);  
-   
+  const { allEvents, isLoading } = useSelector((state) => state.events);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const allEventsData = allEvents ? [...allEvents] : [];
+    const sortedData = allEventsData?.sort((a,b) => b.sold_out - a.sold_out); 
+    const firstFive = sortedData && sortedData.slice(0, 5);
+    setData(firstFive);
+  }, [allEvents]);
+
   return (
     <div>
-     {
-      !isLoading && (
-        <div className={`${styles.section}`}>
-      <div className={`${styles.heading}`}>
-        <h1>Popular Events</h1>
-      </div>
+      {
+        !isLoading && (
+          <div className={`${styles.section}`}>
+            <div className={`${styles.heading}`}>
+              <h1>Popular Events</h1>
+            </div>
 
-      <div className="w-full grid">
-         {
-          allEvents.length !== 0 && (
-            <EventCard data={allEvents && allEvents[0]} />
-          )
-         }
-         <h4>{
-           allEvents?.length === 0 && (
-            'No Events have!'
-           )
-          }
+            <div cclassName="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
+              {
+                data && data.length !== 0 && (
+                  <>
+                    {data && data.map((i, index) => <EventCard data={i} key={index} />)}
+                  </>
+                )
+              }
+            </div>
 
-         </h4>
-      </div>
-     
+          </div>
+        )
+      }
     </div>
-      )
-     }
-  </div>
   )
 }
 
 export default Events
+
